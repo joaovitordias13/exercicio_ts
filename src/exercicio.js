58424ac3790035ee67aeadf7504d3fd38e8e4d68
Joao Vitor Ribeiro <joaovitordias.2b@gmail.com>
2023-09-20 00:05:21 -0300

    Exercicio Concluido

---
+++ src/exercicio.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicacaoNoTs() {
+    let numeroMaior = 10;
+    let numeroMenor = 5;
+    return numeroMenor * numeroMaior;
+}
+function saudacao() {
+    let nome = "João Vitor";
+    return `Olá ${nome}, você está bem`;
+}
---
+++ src/exercicio.ts
@@ -0,0 +1,13 @@
+function multiplicacaoNoTs(): number {
+    let numeroMaior: number = 10
+    let numeroMenor: number = 5
+    return numeroMenor * numeroMaior
+}
+
+    
+function saudacao() {
+    let nome: string = "João Vitor"
+
+    return `Olá ${nome}, você está bem`
+
+}
